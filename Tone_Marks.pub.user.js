367b59725f7e877e06a6bd9abad747f7bc6ffe3c
irrationalpie7 <76858438+irrationalpie7@users.noreply.github.com>
2023-07-20 13:22:17 -0700

    Bump version (no audio)

--- Tone_Marks.pub.user.js
+++ Tone_Marks.pub.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tone Marks
 // @namespace    http://tampermonkey.net/
-// @version      4.5.1
+// @version      4.5.2
 // clang-format off
 // @description  Add tone marks on Ao3 works
 // @author       Cathalinaheart, irrationalpie7
