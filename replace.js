81327002808f25860bcaf4373825ddc452082b0e
Cathalinaheart <105885238+Cathalinaheart@users.noreply.github.com>
2024-05-01 16:14:34 +0200

    Update replace.js

--- replace.js
+++ replace.js
@@ -22,7 +22,7 @@ function capitalize(match, replacement) {
   //if the match is in allcaps and it's not an abbreviation
   if ((match == match.toUpperCase()) && (replacement.length < 2 * match.length)){
     console.log('all caps');
-    return replacement.toUpperCase;
+    return replacement.toUpperCase();
   } else if (match.charAt[0] == match.charAt[0].toUpperCase()) {
     console.log('first letter capital');
     return replacement.charAt[0].toUpperCase() + replacement.substring(1);
